c48a9d697c49507454bb8285336396ad63ab1635
wangxiaodi7 <wangxiaodi1@jd.com>
2019-11-06 21:09:45 +0800

    Netty处理Http请求

--- nettyExercise/src/main/java/com/SimpleClientHandler.java
+++ nettyExercise/src/main/java/com/SimpleClientHandler.java
@@ -16,12 +16,9 @@ public class SimpleClientHandler extends ChannelInboundHandlerAdapter {
      */
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-
         FullHttpResponse response = (FullHttpResponse) msg;
-
         ByteBuf content = response.content();
         HttpHeaders headers = response.headers();
-
         System.out.println("content:" + System.getProperty("line.separator") + content.toString(CharsetUtil.UTF_8));
         System.out.println("headers:" + System.getProperty("line.separator") + headers.toString());
     }
